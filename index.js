b5096d40edf81b1ecdb3b851427da6e13ec5fa76
Anujsoni11 <anujsonisd2@gmail.com>
2026-09-23 16:47:50 +0530

    Final changes

--- index.js
+++ index.js
@@ -1,55 +1,51 @@
-const express = require('express');
-const http = require('http');
-const socketio = require('socket.io');
+const express = require("express");
+const http = require("http");
+const socketio = require("socket.io");
 
-const connect = require('./config/database-config.js');
-
-const Chat = require('./models/chat.js');
+const connect = require('./config/database-config');
+const Chat = require('./models/chat');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-io.on('connection', (socket) => {
+io.on("connection", (socket) => {
     socket.on('join_room', (data) => {
-        console.log('joining a room', data.roomid);
+        console.log("joining a room", data.roomid);
         socket.join(data.roomid);
     });
 
-    socket.on('msg_send', async (data) => {
+    socket.on("msg_send", async (data) => {
         console.log(data);
         const chat = await Chat.create({
             roomId: data.roomid,
             user: data.username,
             content: data.msg
         });
-        io.to(data.roomid).emit('msg_rcvd', data);
+        io.to(data.roomid).emit("msg_rcv", data);
     });
 
     socket.on('typing', (data) => {
-        socket.broadcast.to(data.roomid).emit('someone_typing');
+        socket.broadcast.to(data.roomid).emit("someone_typing");
     })
-
-
 });
 
 app.set('view engine', 'ejs');
 app.use('/', express.static(__dirname + '/public'));
-
 
 app.get('/chat/:roomid', async (req, res) => {
     const chats = await Chat.find({
         roomId: req.params.roomid
     }).select('content user');
     res.render('index', {
-        name: 'Sanket',
+        name: 'Anuj',
         id: req.params.roomid,
         chats: chats
     });
 });
 
 server.listen(3000, async () => {
-    console.log(`Server Started`);
+    console.log("Server is started");
     await connect();
-    console.log('mongo Db Connected');
+    console.log("Mongodb connected");
 });
